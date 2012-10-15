d6212bf0813dd7b1e40ab67f40dcc0cdee4be477
mangodev <sam.rankin@me.com>
2012-10-15 19:34:45 -0400

    withdraw command

--- src/me/mango/fortknox/commands/Withdraw.java
+++ src/me/mango/fortknox/commands/Withdraw.java
@@ -1,9 +1,36 @@
 package me.mango.fortknox.commands;
 
+import me.mango.fortknox.database.DatabaseManager;
+import me.mango.utils.Utils;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class Withdraw {
 	public static void execute(Player sender, String[] args) {
-		
+		if(args.length != 1) {
+			sender.sendMessage(ChatColor.RED + "Correct usage: /withdraw <amount>");
+			return;
+		}
+		int number = Utils.parseInteger(args[0]);
+		if (number < 0) {
+			sender.sendMessage(ChatColor.RED + "That is not a valid number.");
+			return;
+		}
+		if ((DatabaseManager.getBalance(sender.getName()) <= number)) {
+			sender.sendMessage(ChatColor.RED + "You don't have enough ingots stored to withdraw that many!");
+			return;
+		}
+		if ((Utils.freeSpaceForItem(sender.getInventory().getContents(), Material.GOLD_INGOT) <= number)) {
+			sender.sendMessage(ChatColor.RED + "You don't have enough inventory space to withdraw that many!");
+			return;
+		}
+		int newBalance = DatabaseManager.getBalance(sender.getName()) - number;
+		DatabaseManager.updateQuantity(sender.getName(), newBalance);
+		sender.getInventory().addItem(new ItemStack(Material.GOLD_INGOT, number));
+		sender.sendMessage(ChatColor.GREEN + "Successfully withdrew " + number + " ingots.");
+		sender.sendMessage(ChatColor.GREEN + "Your balance is now " + DatabaseManager.getBalance(sender.getName()) + " ingots.");
 	}
 }
--- src/me/mango/utils/Utils.java
+++ src/me/mango/utils/Utils.java
@@ -1,5 +1,8 @@
 package me.mango.utils;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
 public class Utils {
 	public static Integer parseInteger(String s) {
 		try {
@@ -9,5 +12,17 @@ public class Utils {
 			return -1;
 		}
 		return -1;
+	}
+	
+	public static Integer freeSpaceForItem(ItemStack[] i, Material item) {
+		int freeSpace = 0;
+		for (ItemStack stack : i) {
+			if(stack == null) {
+				freeSpace = freeSpace + 64;
+			} else if (stack.getType() == item) {
+				freeSpace = freeSpace + (64 - stack.getAmount());
+			}
+		}
+		return freeSpace;
 	}
 }
