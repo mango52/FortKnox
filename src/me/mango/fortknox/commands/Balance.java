b321261180141a812d337f34fdf16d7e786b874e
mangodev <mango@phoenixgaming.in>
2012-10-24 22:14:34 -0400

    getting balance of other players

--- src/me/mango/fortknox/commands/Balance.java
+++ src/me/mango/fortknox/commands/Balance.java
@@ -8,17 +8,40 @@ import org.bukkit.entity.Player;
 
 public class Balance {
 	public static void execute(Player sender, String[] args) {
+		if ((args.length != 0) && (args.length != 1)) {
+			sender.sendMessage(ChatColor.RED + "Correct usage: /balance [player]");
+			return;
+		}
 		if (!VaultManager.hasPermission(sender, "fortknox.balance")) {
 			sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
 			return;
 		}
-		int balance = DatabaseManager.getBalance(sender.getName());
-		if (balance == 0) {
-			sender.sendMessage(ChatColor.GREEN + "You have no stored ingots.");
-		} else if (balance == 1) {
-			sender.sendMessage(ChatColor.GREEN + "Your current balance is " + balance + " ingot.");
-		} else {
-			sender.sendMessage(ChatColor.GREEN + "Your current balance is " + balance + " ingots.");
+		if (args.length == 1) {
+			if (!VaultManager.hasPermission(sender, "fortknox.balance.others")) {
+				sender.sendMessage(ChatColor.RED + "You don't have permission to check the balance of other players!");
+				return;
+			}
+			int balance = DatabaseManager.getBalance(args[0]);
+			if (balance == 0) {
+				sender.sendMessage(ChatColor.GREEN + args[0] + " has no stored ingots.");
+			} else if (balance == 1) {
+				sender.sendMessage(ChatColor.GREEN + args[0] + " has a balance of " + balance + " ingot.");
+			} else {
+				sender.sendMessage(ChatColor.GREEN + args[0] + " has a balance of " + balance + " ingots.");
+			}
+		} else if (args.length == 0) {
+			if (!VaultManager.hasPermission(sender, "fortknox.balance")) {
+				sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
+				return;
+			}
+			int balance = DatabaseManager.getBalance(sender.getName());
+			if (balance == 0) {
+				sender.sendMessage(ChatColor.GREEN + "You have no stored ingots.");
+			} else if (balance == 1) {
+				sender.sendMessage(ChatColor.GREEN + "Your current balance is " + balance + " ingot.");
+			} else {
+				sender.sendMessage(ChatColor.GREEN + "Your current balance is " + balance + " ingots.");
+			}
 		}
 	}
 }
