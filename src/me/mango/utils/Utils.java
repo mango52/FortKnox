cd1fea085ad0d293548754bf4d51c86d119213db
mangodev <mango@phoenixgaming.in>
2012-10-24 22:02:04 -0400

    minor formatting

--- src/me/mango/utils/Utils.java
+++ src/me/mango/utils/Utils.java
@@ -6,9 +6,9 @@ import org.bukkit.inventory.ItemStack;
 public class Utils {
 	public static int parseInteger(String s) {
 		try {
-			Integer i = Integer.parseInt(s);
-			if(i > 0) return i;
-		} catch(NumberFormatException ex) {
+			int i = Integer.parseInt(s);
+			if (i > 0) return i;
+		} catch (NumberFormatException ex) {
 			return -1;
 		}
 		return -1;
@@ -17,7 +17,7 @@ public class Utils {
 	public static int freeSpaceForItem(ItemStack[] i, Material item) {
 		int freeSpace = 0;
 		for (ItemStack stack : i) {
-			if(stack == null) {
+			if (stack == null) {
 				freeSpace = freeSpace + 64;
 			} else if (stack.getType() == item) {
 				freeSpace = freeSpace + (64 - stack.getAmount());
